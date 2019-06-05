1fa242b5b24c386f6474eba59446b44bdab907f1
treastrain / Tanaka Ryoga <tr@tret.jp>
2019-06-06 03:30:57 +0900

    Initial Commit

---
+++ felica-reader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  felica-reader
+//
+//  Created by treastrain on 2019/06/06.
+//  Copyright © 2019 treastrain / Tanaka Ryoga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
